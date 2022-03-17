84efae9f317a7b3be74374c7909cc8e9e794c39f
christopher coyne <christcoyne@gmail.com>
2022-03-17 16:06:52 -0400

    added basic review cards

--- frontend/src/components/Reviews/components/ReviewsTexts/components/ReviewsText.js
+++ frontend/src/components/Reviews/components/ReviewsTexts/components/ReviewsText.js
@@ -3,12 +3,30 @@ import styles from './ReviewsText.module.css';
 
 const ReviewsText = ({review}) => {
   console.log('review from texts : ', review)
+  const capName = review ? review.name.split(" ").map(str => str[0].toUpperCase() + str.slice(1)).join(" ") : ''
   if (!review) {
     return <></> 
   }
   else {
     return <div className={styles.container}>
+      <h1 className={styles.title}>{capName}</h1>
+      <h2 className={styles.date}>{review.date}</h2>
+      <hr className={styles.break}></hr>
+      <p className={styles.content}>
       {review.content}
+      </p>
+      <h3 className={styles.workloadTitle}>
+        {review.workload ? 'Workload' : ''}
+      </h3>
+        <p className={styles.content}>
+          {review.workload}
+        </p>
+      <button className={styles.agree}>
+        {review.agree} Agree
+      </button>
+      <button className={styles.agree}>
+        {review.disagree} Disagree
+      </button>
     </div>;
   }
 };
