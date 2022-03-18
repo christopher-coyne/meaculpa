181becc3857c29b917e5215e539bccf24d87585f
christopher coyne <christcoyne@gmail.com>
2022-03-18 17:09:57 -0400

    created basic filter drop down

--- frontend/src/components/Reviews/components/ReviewsDropdown/ReviewsDropdown.js
+++ frontend/src/components/Reviews/components/ReviewsDropdown/ReviewsDropdown.js
@@ -2,14 +2,17 @@ import React from 'react';
 import styles from './ReviewsDropdown.module.css'
 import mainLogo from'../../../../assets/comments-solid.svg'
 
-const ReviewsDropdown = () => {
+const ReviewsDropdown = ({setOpen, open, dropState}) => {
+  const toggle = () => {
+    setOpen((open + 1)%2)
+  }
   return <div className={styles.container}>
       <div>
             <span className={styles.reviewNumber}>
                 <img src={mainLogo} alt="comments" className={styles.image}/>
               35 Reviews
             </span>
-          <span className={styles.reviewFilter}>Sort By: most recent</span>
+          <span className={styles.reviewFilter} onClick={() => toggle()}>Sort By: {dropState} V</span>
       </div>
       <button className={styles.addReview}>
         + Add Review
