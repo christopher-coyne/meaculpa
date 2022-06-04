943e26e0036ead0f1fdf179356cc6ff24812afa1
christopher coyne <christcoyne@gmail.com>
2022-06-03 20:25:40 -0400

    agree and disagree colors added

--- frontend/src/components/Reviews/components/ReviewsTexts/components/ReviewsText.js
+++ frontend/src/components/Reviews/components/ReviewsTexts/components/ReviewsText.js
@@ -1,8 +1,14 @@
-import { React, useState } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import styles from "./ReviewsText.module.css";
 import ExpandButton from "../../../../ExpandButton/ExpandButton";
 
 const ReviewsText = ({ review }) => {
+  const [height, setHeight] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight);
+  });
   // console.log('review from texts : ', review)
   const [expanded, setExpanded] = useState(false);
   const capName = review.name
@@ -19,7 +25,11 @@ const ReviewsText = ({ review }) => {
     return <></>;
   } else {
     return (
-      <div className={`${styles.container} ${expanded && styles.expanded}`}>
+      <div
+        className={`${styles.container} ${expanded && styles.expanded}`}
+        ref={ref}
+      >
+        <div>{height}</div>
         <h1 className={styles.title}>{capName}</h1>
         <h2 className={styles.date}>{review.date}</h2>
         <hr className={styles.break}></hr>
@@ -28,8 +38,10 @@ const ReviewsText = ({ review }) => {
           {review.workload ? "Workload" : ""}
         </h3>
         <p className={styles.content}>{review.workload}</p>
-        <button className={styles.agree}>{review.agree} Agree</button>
-        <button className={styles.agree}>{review.disagree} Disagree</button>
+        <p className={styles.reactions}>
+          <span className={styles.agree}>{review.agree} Agree</span>{" "}
+          <span className={styles.disagree}>{review.disagree} Disagree</span>
+        </p>
         <ExpandButton
           changeExpand={() => changeExpanded()}
           expanded={expanded}
