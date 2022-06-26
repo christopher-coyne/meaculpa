7647d98086baef6fae203a1d18600ffb77e49904
christopher coyne <christcoyne@gmail.com>
2022-06-25 22:38:27 -0400

    refactor review dropdown

--- frontend/src/components/Reviews/components/ReviewsDropdown/ReviewsDropdown.js
+++ frontend/src/components/Reviews/components/ReviewsDropdown/ReviewsDropdown.js
@@ -1,24 +1,28 @@
 import React from "react";
 import styles from "./ReviewsDropdown.module.css";
-import mainLogo from "../../../../assets/comments-solid.svg";
 
-const ReviewsDropdown = ({ setOpen, open, dropState, numReviews }) => {
-  const toggle = () => {
-    setOpen((open + 1) % 2);
-  };
+const options = ["Recent", "Controversial", "Highest Rated"];
+
+const ReviewsDropdown = ({ open, select, dropState }) => {
   return (
-    <div className={styles.container}>
-      <div>
-        <span className={styles.reviewNumber}>
-          <img src={mainLogo} alt="comments" className={styles.image} />
-          {numReviews} Reviews
-        </span>
-        <span className={styles.reviewFilter} onClick={() => toggle()}>
-          Sort By: {dropState} V
-        </span>
-      </div>
-      <button className={styles.addReview}>+ Add Review</button>
-    </div>
+    <>
+      {open === 1 ? (
+        <ul className={styles.dropdown}>
+          {options.map((option) => {
+            if (option !== dropState) {
+              return (
+                <li className={styles.option} onClick={() => select(option)}>
+                  {option}
+                </li>
+              );
+            }
+            return <></>;
+          })}
+        </ul>
+      ) : (
+        <> </>
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/components/Reviews/components/ReviewsMenu/ReviewsMenu.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./ReviewsMenu.module.css";
+import mainLogo from "../../../../assets/comments-solid.svg";
+
+const ReviewsDropdown = ({ setOpen, open, dropState, numReviews }) => {
+  const toggle = () => {
+    setOpen((open + 1) % 2);
+  };
+  return (
+    <div className={styles.container}>
+      <div>
+        <span className={styles.reviewNumber}>
+          <img src={mainLogo} alt="comments" className={styles.image} />
+          {numReviews} Reviews
+        </span>
+        <span className={styles.reviewFilter} onClick={() => toggle()}>
+          Sort By: {dropState} V
+        </span>
+      </div>
+      <button className={styles.addReview}>+ Add Review</button>
+    </div>
+  );
+};
+
+export default ReviewsDropdown;
