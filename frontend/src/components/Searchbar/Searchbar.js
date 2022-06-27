29baff3a6ff10565ee5b932ded2a50546c0eb554
christopher coyne <christcoyne@gmail.com>
2022-06-26 22:10:46 -0400

    removed excess comments + code

--- frontend/src/components/Searchbar/Searchbar.js
+++ frontend/src/components/Searchbar/Searchbar.js
@@ -1,46 +1,23 @@
 import { React, useEffect, useState } from "react";
 import styles from "./Searchbar.module.css";
-import magnifyingGlass from "../../assets/search-solid.svg";
 import Result from "./components/Result";
 import axios from "axios";
 
 const Searchbar = () => {
   // const [userInput, setUserInput] = useState('');
   const [filteredData, setFilteredData] = useState([]);
-
-  const fetchData = async () => {
-    const { data } = await axios.get("/search-prof-name/adam");
-    console.log("data ", data);
-    return data;
-  };
   const printInput = async (e) => {
-    if (!e || e.length <= 3) {
+    if (!e || e.length < 1) {
       setFilteredData([]);
       return;
     }
 
     // don't search until the user enters in at least 4 letters
-    if (e.length >= 3) {
-      console.log("input e : ", e);
-      const { data } = await axios.get(`/match-term-all/${e}`);
-      console.log("results from search : ", data.results);
-      setFilteredData(data.results);
-    }
+    console.log("input e : ", e);
+    const { data } = await axios.get(`/match-term-all/${e}`);
+    console.log("results from search : ", data.results);
+    setFilteredData(data.results);
   };
-
-  /*
-  const printInput = (e) => {
-    if (!e) {
-      setFilteredData([]);
-      return;
-    }
-    console.log("input e : ", e)
-    const data = testData.filter(d => {
-      return d.includes(e)
-    })
-    setFilteredData(data);
-  }
-  */
 
   return (
     <>
