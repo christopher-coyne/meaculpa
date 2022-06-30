815337612731f7055383272b474403fa71b7b7da
christopher coyne <christcoyne@gmail.com>
2022-06-29 20:04:44 -0400

    custom hooks created

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+
+const useFetch = () => {
+  return;
+};
+
+export default useFetch;
---
+++ frontend/src/hooks/useFilteredData.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+/* searches API on key change, sets state to anything that matches input */
+const useFilteredData = () => {
+  const [filteredData, setFilteredData] = useState([]);
+  const fetchAndSetFilteredData = async (url, searchVal) => {
+    if (!searchVal || searchVal.length < 1) {
+      setFilteredData([]);
+      return;
+    }
+    const { data } = await axios.get(url);
+    console.log("data : ", data);
+    setFilteredData(data.results);
+  };
+
+  return [filteredData, fetchAndSetFilteredData];
+};
+
+export default useFilteredData;
