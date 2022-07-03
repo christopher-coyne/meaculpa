27acdcea9bccad72e3d3ebbc2bea82767cc84e1e
christopher coyne <christcoyne@gmail.com>
2022-07-03 12:03:07 -0400

    click outside of an element context

--- frontend/src/components/ClickOutsideContext/ClickOutsideContext.js
+++ frontend/src/components/ClickOutsideContext/ClickOutsideContext.js
@@ -1,15 +1,15 @@
-import React, { useContext, useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 export const OutsideContext = React.createContext();
 export const ToggleDropDown = React.createContext();
 
 const ClickOutsideContext = ({ children }) => {
-  const [dropDownOpen, setDropDownOpen] = useState(0);
+  const [dropDownOpen, setDropDownOpen] = useState(false);
   useEffect(() => {
     document.addEventListener("mousedown", (event) => {
       if (clickRef.current && clickRef.current.contains) {
         if (!clickRef.current.contains(event.target)) {
           setDropDownOpen(false);
-          console.log("CLICK OUTSIDE!!!");
+          // console.log("CLICK OUTSIDE!!!");
         }
       }
     });
