7de092e3145116620ea0ed7fab03487a2d654c86
christopher coyne <christcoyne@gmail.com>
2022-07-02 23:36:11 -0400

    click outside context hook added

---
+++ frontend/src/components/ClickOutsideContext/ClickOutsideContext.js
@@ -0,0 +1,29 @@
+import React, { useContext, useRef, useState, useEffect } from "react";
+export const OutsideContext = React.createContext();
+export const ToggleDropDown = React.createContext();
+
+const ClickOutsideContext = ({ children }) => {
+  const [dropDownOpen, setDropDownOpen] = useState(0);
+  useEffect(() => {
+    document.addEventListener("mousedown", (event) => {
+      if (clickRef.current && clickRef.current.contains) {
+        if (!clickRef.current.contains(event.target)) {
+          setDropDownOpen(false);
+          console.log("CLICK OUTSIDE!!!");
+        }
+      }
+    });
+  });
+  const clickRef = useRef("myref");
+  return (
+    <>
+      <OutsideContext.Provider value={clickRef}>
+        <ToggleDropDown.Provider value={{ dropDownOpen, setDropDownOpen }}>
+          {children}
+        </ToggleDropDown.Provider>
+      </OutsideContext.Provider>
+    </>
+  );
+};
+
+export default ClickOutsideContext;
