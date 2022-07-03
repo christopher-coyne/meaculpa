4e11b461ebe2d4fe53e4e372e545bd374fa196c1
christopher coyne <christcoyne@gmail.com>
2022-07-03 12:03:17 -0400

    removed comments

--- frontend/src/components/Graphs/BarChart/BarChart.js
+++ frontend/src/components/Graphs/BarChart/BarChart.js
@@ -11,36 +11,6 @@ import {
   Legend,
 } from "chart.js";
 
-/*
-const BarChart = ({data, width, height, margins}) => {
-    const innerHeight = height - margins.top - margins.bottom
-    const innerWidth = width - margins.left - margins.right
-    const yScale = scaleLinear().domain([0, max(data.map(d => parseFloat(d.count)))]).range([0, innerHeight-40])
-
-    const xScale = scaleBand().domain(data.map(d=> d.name)).range([0, innerWidth]).padding(.15)
-    console.log('xscale bandwidth : ', xScale.bandwidth())
-    console.log('y ticks : ', yScale.ticks())
-  return <svg width={`70%`} className={styles.chart} viewBox="0 0 900 500">
-    <g transform={`translate(${margins.left}, ${margins.top})`}>
-    {yScale.ticks().map(tickValue => (
-        <g key={tickValue} transform={`translate(0,${yScale(tickValue)})`}>
-        <line x1={15} x2={innerWidth} y1={-3} y2={-3} stroke="black"/>
-        <text style={{textAnchor:"middle"}}>{tickValue}</text>
-        </g>
-      ))}
-      {xScale.domain().map(tickValue => (
-        <g key={tickValue} transform={`translate(${xScale(tickValue) + (xScale.bandwidth() / 2)},${innerHeight + 20})`}>
-        <text style={{textAnchor:"middle"}}>{tickValue}</text>
-        </g>
-      ))}
-      {data.map((d, i) => <rect key={i} width={xScale.bandwidth()}  height={yScale(d.count)} y={innerHeight - yScale(d.count) - 20} x={xScale(d.name)}/>)}
-    </g>
-  </svg>;
-};
-
-export default BarChart;
-*/
-
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -136,15 +106,3 @@ const BarChart = ({ data, selected }) => {
 };
 
 export default BarChart;
-
-/*
-  return (
-  <Bar data={{labels: labels, datasets: datasets}}
-  options = {{
-    responsive: true,
-    maintainAspectRatio: true,
-  }}
-  />
-  )
-};
-*/
